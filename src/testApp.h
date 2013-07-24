cd00bc038e395eac5f54d463f1a29d3ee345e125
James George <james@jamesgeorge.org>
2013-07-24 17:45:45 -0400

    rename to conform to naming convention

--- src/testApp.h
+++ src/testApp.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
-#include "emptyVisualSystem.h"
+#include "CloudsVisualSystemEmpty.h"
 
 class testApp : public ofBaseApp{
   public:
@@ -20,5 +20,5 @@ class testApp : public ofBaseApp{
 	void gotMessage(ofMessage msg);
 	void exit();
 		
-	emptyVisualSystem visualSystem;
+	CloudsVisualSystemEmpty visualSystem;
 };
---
+++ vs_src/CloudsVisualSystemEmpty.cpp
@@ -0,0 +1,125 @@
+//
+//  CloudsVisualSystemEmpty.cpp
+//
+
+#include "CloudsVisualSystemEmpty.h"
+
+void CloudsVisualSystemEmpty::selfSetup()
+{
+    
+}
+
+void CloudsVisualSystemEmpty::selfPresetLoaded(string presetPath){
+	
+}
+
+void CloudsVisualSystemEmpty::selfBegin()
+{
+    
+}
+
+void CloudsVisualSystemEmpty::selfEnd()
+{
+    
+}
+
+void CloudsVisualSystemEmpty::selfExit()
+{
+    
+}
+
+void CloudsVisualSystemEmpty::selfSetupSystemGui()
+{
+   
+}
+
+void CloudsVisualSystemEmpty::selfSetupRenderGui()
+{
+    
+}
+
+void CloudsVisualSystemEmpty::guiSystemEvent(ofxUIEventArgs &e)
+{
+    
+}
+
+void CloudsVisualSystemEmpty::selfKeyPressed(ofKeyEventArgs & args){
+    
+}
+
+void CloudsVisualSystemEmpty::selfUpdate()
+{
+    
+}
+
+void CloudsVisualSystemEmpty::selfDraw()
+{
+
+}
+
+
+void CloudsVisualSystemEmpty::selfSetupGuis()
+{
+    
+}
+
+void CloudsVisualSystemEmpty::selfAutoMode()
+{
+    
+}
+
+
+void CloudsVisualSystemEmpty::selfDrawBackground()
+{
+    
+}
+
+void CloudsVisualSystemEmpty::selfDrawDebug()
+{
+    
+}
+
+void CloudsVisualSystemEmpty::selfSceneTransformation()
+{
+    
+}
+
+void CloudsVisualSystemEmpty::selfKeyReleased(ofKeyEventArgs & args)
+{
+    
+}
+
+void CloudsVisualSystemEmpty::selfMouseDragged(ofMouseEventArgs& data)
+{
+    
+}
+
+void CloudsVisualSystemEmpty::selfMouseMoved(ofMouseEventArgs& data)
+{
+    
+}
+
+void CloudsVisualSystemEmpty::selfMousePressed(ofMouseEventArgs& data)
+{
+    
+}
+
+void CloudsVisualSystemEmpty::selfMouseReleased(ofMouseEventArgs& data)
+{
+    
+}
+
+void CloudsVisualSystemEmpty::selfSetupGui()
+{
+    
+}
+
+void CloudsVisualSystemEmpty::selfGuiEvent(ofxUIEventArgs &e)
+{
+    
+}
+
+void CloudsVisualSystemEmpty::guiRenderEvent(ofxUIEventArgs &e)
+{
+    
+}
